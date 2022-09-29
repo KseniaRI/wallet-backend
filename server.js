07f2c39540225ee73af479934dcc00e3480f6f2f
Ksenia Pilshchikova <ksushapi@gmail.com>
2022-09-29 10:54:16 +0200

    create hw-2 branch

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const app = require('./app')
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
+
+// NEvlkPqwRSqFJgFZ
+// Ksenia
+// mongodb+srv://Ksenia:NEvlkPqwRSqFJgFZ@cluster0.xbsrtyb.mongodb.net/test

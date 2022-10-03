641a597e862b4a12703e8d02954f5a847a96c170
Ksenia Pilshchikova <ksushapi@gmail.com>
2022-10-03 12:20:01 +0200

    conect to mongodb and use mongoose operations

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const contactsRouter = require('./routes/api/contacts');
+const dotenv = require('dotenv');
 
-// NEvlkPqwRSqFJgFZ
-// Ksenia
-// mongodb+srv://Ksenia:NEvlkPqwRSqFJgFZ@cluster0.xbsrtyb.mongodb.net/test
+dotenv.config();
 
-const contactsRouter = require('./routes/api/contacts')
-
-const app = express()
+const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
@@ -16,7 +14,7 @@ app.use(logger(formatsLogger))
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/contacts', contactsRouter);
 
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Not found' })
@@ -28,4 +26,4 @@ app.use((err, req, res, next) => {
 
 })
 
-module.exports = app
+module.exports = app;
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const { validation, ctrlWrapper } = require('../../middlewares/index');
+const { joiSchema, statusJoiSchema } = require('../../models/contacts');
+const { contactsCtrl: ctrl } = require('../../controllers');
 
-const contactValidation = require('../../middlewares/validationMiddleware');
-const { listContacts, getContactById, addContact, updateContact, removeContact } = require('../../models/contacts');
+const validationMiddleware = validation(joiSchema);
+const statusValidationMiddleware = validation(statusJoiSchema);
 
 const router = express.Router()
 
-router.get('/', listContacts);
-router.get('/:contactId', getContactById);
-router.post('/', contactValidation, addContact);
-router.delete('/:contactId', removeContact);
-router.put('/:contactId', contactValidation, updateContact)
+router.get('/', ctrlWrapper(ctrl.listContacts));
+router.get('/:contactId', ctrlWrapper(ctrl.getContactById));
+router.post('/', validationMiddleware, ctrlWrapper(ctrl.addContact));
+router.delete('/:contactId', ctrlWrapper(ctrl.removeContact));
+router.put('/:contactId', validationMiddleware, ctrlWrapper(ctrl.updateContact));
+router.patch('/:contactId/favorite', statusValidationMiddleware, ctrlWrapper(ctrl.updateStatusContact))
 
 module.exports = router;

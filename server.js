8076e1077b9761eb2843c576353c1ead97ae0be2
Ksenia Pilshchikova <ksushapi@gmail.com>
2022-11-30 21:06:23 +0100

    port change

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const { DB_HOST, PORT = 3001} = process.env;
 
 mongoose.connect(DB_HOST)
   .then(() => {
     app.listen(PORT);
-    console.log("Server running. Use our API on port: 3000");
+    console.log("Server running. Use our API on port: 3001");
     console.log("Database connection successful");
   })
   .catch(error => {

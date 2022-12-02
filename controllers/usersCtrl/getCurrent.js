839cac2739a80e9bd72934df088568b3c6526bfd
Ksenia Pilshchikova <ksushapi@gmail.com>
2022-12-02 19:13:26 +0100

    Update getCurrent.js

--- controllers/usersCtrl/getCurrent.js
+++ controllers/usersCtrl/getCurrent.js
@@ -1,12 +1,13 @@
 const getCurrent = async (req, res, next) => {
-    const { email, name} = req.user;
+    const { email, name, balance} = req.user;
     res.json({
         status: "success",
         code: 200,
         data: {
             user: {
                 email,
-                name
+                name,
+                balance
             }
         }
     })

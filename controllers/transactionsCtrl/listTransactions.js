747fcd4476baa46ba79b07c5bf6cad3b268c510b
Ksenia Pilshchikova <ksushapi@gmail.com>
2022-12-05 13:02:25 +0100

    Update listTransactions.js

--- controllers/transactionsCtrl/listTransactions.js
+++ controllers/transactionsCtrl/listTransactions.js
@@ -4,10 +4,9 @@ const listTransactions = async (req, res) => {
   const { _id } = req.user;
   // const { page = 1, limit = 10} = req.query;
   // const skip = (page - 1) * limit;
-  
-  const transactions = await Transaction.find({ owner: _id }
-    // , "", {skip, limit: Number(limit)}
-  ).populate("owner", "_id email");
+  // , "", {skip, limit: Number(limit)}
+
+  const transactions = await Transaction.find({ owner: _id }).populate("owner", "_id email");
   return res.json({
     status: "success",
     code: 200,
